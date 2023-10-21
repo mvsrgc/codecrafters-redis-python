666dcbd8a8bbb8c4db9a67f2a39dd462b4717b3a
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2023-10-21 07:45:31 -0400

    Respond with PONG

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 # Uncomment this to pass the first stage
 import socket
+from dataclasses import dataclass
+
+@dataclass
+class SimpleString:
+    raw: str
+
+    def __str__(self):
+        stripped_raw = self.raw.replace("\r", "").replace("\n", "")
+        return f"+{stripped_raw}\r\n"
 
 
 def main():
@@ -9,7 +18,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    hello_message = SimpleString("PONG")
+    client_socket, _ = server_socket.accept() # wait for client
+    client_socket.send(str(hello_message).encode())
 
 
 if __name__ == "__main__":
